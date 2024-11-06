65ef33f17661bd43ab114cb0781beedee6dd3615
a.borisov <ksandr.mobile@gmail.com>
2024-11-07 02:57:16 +0300

    Добавлен аттрибут static для исправления ошибки с множественным объявлением name##Strings

--- include/CEFLogMacro.h
+++ include/CEFLogMacro.h
@@ -2,16 +2,18 @@
 
 #include <boost/preprocessor.hpp>
 
-#define PROCESS_ONE_ELEMENT(r, unused, idx, elem)                              \
-    BOOST_PP_COMMA_IF(idx) BOOST_PP_STRINGIZE(elem)
+#define PROCESS_ONE_ELEMENT(r, unused, idx, elem) \
+    BOOST_PP_COMMA_IF(idx)                        \
+    BOOST_PP_STRINGIZE(elem)
 
-#define ENUM_CLASS_MACRO(name, ...)                                            \
-    enum class name {                                                          \
-        __VA_ARGS__                                                            \
-    };                                                                         \
-    const char *name##Strings[] = {BOOST_PP_SEQ_FOR_EACH_I(                    \
-        PROCESS_ONE_ELEMENT, % %, BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__))};     \
-    template <typename T> constexpr const char *name##ToString(T value)        \
-    {                                                                          \
-        return name##Strings[static_cast<int>(value)];                         \
+#define ENUM_CLASS_MACRO(name, ...)                                        \
+    enum class name {                                                      \
+        __VA_ARGS__                                                        \
+    };                                                                     \
+    static const char *name##Strings[] = {BOOST_PP_SEQ_FOR_EACH_I(         \
+        PROCESS_ONE_ELEMENT, % %, BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__))}; \
+    template <typename T>                                                  \
+    constexpr const char *name##ToString(T value)                          \
+    {                                                                      \
+        return name##Strings[static_cast<int>(value)];                     \
     }
