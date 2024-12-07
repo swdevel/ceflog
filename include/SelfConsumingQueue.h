ff8a59de975c45c32c52953ba6be4bc7c4ce5741
a.borisov <ksandr.mobile@gmail.com>
2024-12-07 18:05:29 +0300

    Добавлены комментарии для генерации документации в формате doxygen

--- include/SelfConsumingQueue.h
+++ include/SelfConsumingQueue.h
@@ -1,13 +1,14 @@
 /**
  * @file SelfConsumingQueue.h
  * @author Alexander Borisov
- * @brief Файл содержит объявление класса очереди с функцией автоматический асинхронной обработки
+ * @brief Файл содержит объявление класса очереди для автоматической асинхронной обработки элементов
  * @version 0.1
  * @date 2024-12-07
  *
  * @copyright Copyright (c) 2024
  *
  */
+
 #pragma once
 
 #include <atomic>
@@ -21,12 +22,28 @@
 namespace ceflog::syslog
 {
 
+/**
+ * @brief Класс очереди для автоматической асинхронной обработки элементов
+ *
+ * Функционал класса позволяет асинхронно выполнять операции добавления и обработки элементов очереди.
+ * Для обработки каждого из элементов очереди будет вызвана callback функция, передаваемая в класс извне.
+ * Присутствует возможность ограничивать скорость обработки элементов очереди за счёт ограничения на
+ * максимально допустимое количество вызовов callback функции за одну секунду.
+ *
+ * @tparam T Тип элементов очереди
+ */
 template <typename T>
 class SelfConsumingQueue
 {
 public:
     SelfConsumingQueue() = delete;
 
+    /**
+     * @brief Конструктор класса
+     *
+     * @param callback Константный указатель на callback функцию, которая будет вызвана для обработки каждого из элементов очереди
+     * @param processingLimitPerSecond Максимально допустимое количество вызовов callback функции за одну секунду
+     */
     SelfConsumingQueue(const std::function<void(T&)>& callback,
                        const uint32_t processingLimitPerSecond)
         : callback(callback),
@@ -56,22 +73,38 @@ public:
     SelfConsumingQueue(SelfConsumingQueue&& move) noexcept = delete;
     SelfConsumingQueue& operator=(SelfConsumingQueue&& move) noexcept = delete;
 
+    /**
+     * @brief Метод для установки максимально допустимого количества вызовов callback функции за одну секунду
+     *
+     * @param value Максимально допустимое количество вызовов callback функции за одну секунду
+     */
     void SetProcessingLimitPerSecond(const uint32_t value) noexcept
     {
         limit = value;
     }
 
+    /**
+     * @brief Метод для получения максимально допустимого количества вызовов callback функции за одну секунду
+     *
+     * @return uint32_t Максимально допустимое количество вызовов callback функции за одну секунду
+     */
     uint32_t GetProcessingLimitPerSecond() const noexcept
     {
         return limit;
     }
 
+    /**
+     * @brief Метод для добавления нового элемента в очередь
+     *
+     * @param element Константная ссылка на элемент
+     */
     void Push(const T& element)
     {
         input.push(element);
         available.notify_one();
     }
 
+private:
     void Worker()
     {
         while (running) {
@@ -85,7 +118,6 @@ public:
         }
     }
 
-private:
     void Flush()
     {
         output.swap(input);
